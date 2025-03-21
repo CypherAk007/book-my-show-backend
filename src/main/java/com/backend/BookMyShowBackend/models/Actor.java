e6a1f4ee48d319a90b8fcd0bea64dc5d8ff16a02
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2025-03-21 20:51:32 +0530

    adding cardinality mapping betwen tables

--- src/main/java/com/backend/BookMyShowBackend/models/Actor.java
+++ src/main/java/com/backend/BookMyShowBackend/models/Actor.java
@@ -1,10 +1,12 @@
 package com.backend.BookMyShowBackend.models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Actor extends BaseModel{
     private String name;
 }
--- src/main/java/com/backend/BookMyShowBackend/models/Booking.java
+++ src/main/java/com/backend/BookMyShowBackend/models/Booking.java
@@ -1,6 +1,7 @@
 package com.backend.BookMyShowBackend.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,15 @@ import java.util.List;
 @Entity
 public class Booking extends BaseModel{
     private BookingStatus status;
+//    1 booking many show seats
+//    1 show seat had to be one booking but
+//            we can cancle and other booking
+//            can be made on same showseat
+
+    @ManyToMany
     private List<ShowSeat> showSeats;
+
+
     private User user;
     private Date time;
     private MovieShow movieShow;
